52cd31c82d5d38934a1fbd4c2a912826f21b864d
Chris <ChrisButler296@gmail.com>
2014-04-26 23:02:34 -0600

    Updated symbol table test file. A couple changes to messages printed and some stylistic changes: nullptr over 0, C++ comments on new lines over C comments on the same, etc.

--- tests/symboltable_test.cpp
+++ tests/symboltable_test.cpp
@@ -1,31 +1,44 @@
-#include <cassert>
-#include <stdio.h>
-
 #include "symboltable.h"
+
+#include <cassert>
+#include <cstdio>
 
 int main() {
     SymbolTable st1(0);
     SymbolTableNode *bad = st1.retrieveSymbol("a");
-    assert(bad == 0);               /* Invalid symbols should not return a valid symbol node */
+    // Invalid symbols should not return a valid symbol node
+    assert(bad == nullptr);
+
     // NOTE: Symbol table does not create an initial scope
     st1.openScope();
     bad = st1.retrieveSymbol("a");
-    assert(bad == 0);               /* Invalid symbols should not return a valid symbol node */
+    // Invalid symbols should not return a valid symbol node
+    assert(bad == nullptr);
     st1.enterSymbol("a", "string", {});
+
     SymbolTableNode* a = st1.retrieveSymbol("a");
-    assert(a != 0);                 /* Make sure that enterSymbol works correctly */
-    printf("made it here\n");
-    assert(a->type == "string");    /* Test that retrieveSymbol returns the correct symbol */
+    // Make sure that enterSymbol works correctly
+    assert(a != nullptr);
+    // Test that retrieveSymbol returns the correct symbol
+    assert(a->type == "string");
+    printf("Symbol table properly retrieves existing symbols.\n");
+
+
     st1.openScope();
     st1.enterSymbol("x", "int", {});
     SymbolTableNode* hasx = st1.retrieveSymbol("x");
-    assert(hasx != 0);              /* Make sure that enterSymbol works correctly inside scopes */
-    assert(hasx->type == "int");    /* Test that retrieveSymbol returns the correct symbol */
+    // Make sure that enterSymbol works correctly inside scopes
+    assert(hasx != nullptr);
+    // Test that retrieveSymbol returns the correct symbol
+    assert(hasx->type == "int");
     st1.closeScope();
+
     SymbolTableNode *stnode = st1.retrieveSymbol("x");
-    assert(stnode == 0);            /* Test that closing the scope loses access to variables created within */
+    // Test that closing the scope loses access to variables created within
+    assert(stnode == nullptr);
     SymbolTableNode *again = st1.retrieveSymbol("a");
-    assert(again != 0);             /* Make sure that symbols survive across scopes */
+    // Make sure that symbols survive across scopes
+    assert(again != nullptr);
 
     SymbolTable st2(0);
     st2.openScope();
@@ -35,22 +48,28 @@ int main() {
     st2.openScope();
     st2.enterSymbol("three", "int", {});
     SymbolTableNode *deep = st2.retrieveSymbol("one");
-    assert(deep != 0);              /* Make sure "deeply" nested scopes maintain access to outer scopes */
+    // Make sure "deeply" nested scopes maintain access to outer scopes
+    assert(deep != nullptr);
     deep = st2.retrieveSymbol("two");
-    assert(deep != 0);              /* Make sure access is maintained to outer scopes */
+    // Make sure access is maintained to outer scopes
+    assert(deep != nullptr);
     deep = st2.retrieveSymbol("three");
-    assert(deep != 0);              /* Make sure "deeply" nested scopes still allow symbol creation */
+    // Make sure "deeply" nested scopes still allow symbol creation
+    assert(deep != nullptr);
     st2.closeScope();
-    assert(deep != 0);              /* TODO I'm not actually sure if this should be bad or not */
+    // TODO I'm not actually sure if this should be bad or not
+    assert(deep != nullptr);
     deep = st2.retrieveSymbol("three");
-    assert(deep == 0);              /* Make sure that the pointer actually changes when trying to get a bad pointer */
+    // Make sure that the pointer actually changes when trying to get a bad pointer
+    assert(deep == nullptr);
     deep = st2.retrieveSymbol("two");
     st2.closeScope();
-    assert(deep != 0);              /* TODO I'm not actually sure if this should be bad or not */
+    // TODO I'm not actually sure if this should be bad or not
+    assert(deep != nullptr);
     deep = st2.retrieveSymbol("two");
-    assert(deep == 0);
+    assert(deep == nullptr);
     deep = st2.retrieveSymbol("one");
-    assert(deep != 0);
+    assert(deep != nullptr);
 
     return 0;
 }
